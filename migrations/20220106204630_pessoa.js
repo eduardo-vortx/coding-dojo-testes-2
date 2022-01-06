6f4d799668913d38ba695e64b9a712dde3aa93db
Pedro Haupenthal <pfh@vortx.com.br>
2022-01-06 17:55:56 -0300

    add migration

--- migrations/20220106204630_pessoa.js
+++ migrations/20220106204630_pessoa.js
@@ -1,10 +1,15 @@
 
-exports.up = function(knex) {
+exports.up = async function(knex) {
+
     return knex.schema.createTable('pessoas', table => {
-        
+        table.increments('id')
+        table.string('email', 100).notNullable()
+        table.string('genero', 1).notNullable()
+        table.string('nomeCompleto', 150).notNullable()
+        table.string('celular', 20)
     })  
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('pessoas')
 };
